d4d3f303b2526c95b96236e0ad952c937a18a26f
John Wesley Florentino Diniz <johnwesleyfd@gmail.com>
2024-05-02 17:07:33 -0300

    criação

--- Assets/Scripts/avião.cs
+++ Assets/Scripts/avião.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class Aviao : MonoBehaviour
 {
 	Rigidbody2D fisica;
+	[Serialize] private float forca = 6;
+	private Diretor diretor;
+	private Vector3 posicaoInicial;
 
 	void Awake()
 	{
+		this.posicaoInicial = this.transform.position;
     	this.fisica = this.GetComponent<Rigidbody2D>();
 	}
+
+	private void Start()
+    {
+        this.diretor = GameObject.FindObjectOfType<Diretor>();  // Coletar um objeto o qual está em outro objeto
+    } 
+
 	// Update is called once per frame
 	void Update()
 	{
@@ -21,11 +31,19 @@ public class Aviao : MonoBehaviour
 
 	void Impulsionar()
 	{
-    	this.fisica.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+		this.fisica.velocity = Vector2.zero;
+    	this.fisica.AddForce(Vector2.up * this.forca, ForceMode2D.Impulse);
 	}
 
 	private void OnCollisionenter2D(Collision colisao){
 		this.fisica.simulated = false;
+		this.diretor.FinalizarJogo();
 	}
+
+	public void Reiniciar()
+    {
+        this.transform.position = this.posicaoInicial;
+        this.fisica.simulated = true;
+    }
 }
 
